c8cbffe3d660ad0394ec93b48f7428e848ca6bca
manjushareddy143 <manjushamac@gmail.com>
2023-05-01 18:27:30 +0100

    add products from data

--- fronend/src/data.js
+++ fronend/src/data.js
@@ -1,35 +1,39 @@
-products:[
-    {
-        _id:'1',
-        name:'Nike ',
-        category:'Shrits',
-        image:'images/d1.jpg',
-        price:120,
-        brand:'Nike',
-        NewReview:10,
-        description:'Hig quailty'
-
-    },
-    {
-        _id:'2',
-        name:'Nike ',
-        category:'Shrits',
-        image:'images/d1.jpg',
-        price:120,
-        brand:'Nike',
-        NewReview:10,
-        description:'Hig quailty'
-
-    },
-    {
-        _id:'3',
-        name:'Nike ',
-        category:'Shrits',
-        image:'images/d1.jpg',
-        price:120,
-        brand:'Nike',
-        NewReview:10,
-        description:'Hig quailty'
-
+const data={
+    products:[
+        {
+            id:'1',
+            name:'Nike ',
+            category:'Shrits',
+            image:'images/d1.jpg',
+            price:120,
+            brand:'Nike',
+            NewReview:10,
+            description:'Hig quailty'
+    
+        },
+        {
+            id:'2',
+            name:'Nike ',
+            category:'Shrits',
+            image:'images/d1.jpg',
+            price:120,
+            brand:'Nike',
+            NewReview:10,
+            description:'Hig quailty'
+    
+        },
+        {
+            id:'3',
+            name:'Nike ',
+            category:'Shrits',
+            image:'images/d1.jpg',
+            price:120,
+            brand:'Nike',
+            NewReview:10,
+            description:'Hig quailty'
+    
+        }
+    ]
     }
-]
+export default data;
+ 
