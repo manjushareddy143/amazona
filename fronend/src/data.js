e99f225012d1f88938a388d1a7923b1f07dddcbc
manjushareddy143 <manjushamac@gmail.com>
2023-05-01 13:45:26 +0100

    first commit

---
+++ fronend/src/data.js
@@ -0,0 +1,35 @@
+products:[
+    {
+        _id:'1',
+        name:'Nike ',
+        category:'Shrits',
+        image:'images/d1.jpg',
+        price:120,
+        brand:'Nike',
+        NewReview:10,
+        description:'Hig quailty'
+
+    },
+    {
+        _id:'2',
+        name:'Nike ',
+        category:'Shrits',
+        image:'images/d1.jpg',
+        price:120,
+        brand:'Nike',
+        NewReview:10,
+        description:'Hig quailty'
+
+    },
+    {
+        _id:'3',
+        name:'Nike ',
+        category:'Shrits',
+        image:'images/d1.jpg',
+        price:120,
+        brand:'Nike',
+        NewReview:10,
+        description:'Hig quailty'
+
+    }
+]
